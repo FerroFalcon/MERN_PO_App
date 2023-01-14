84d0babe0718428c9b799117c420d32f6f71234a
Jaideep Ghungharwal <jdghungharwal04@gmail.com>
2023-01-14 11:26:03 +0530

    project complete

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,21 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+//
 
-// 
+const DB =
+  "mongodb+srv://user404:ZgXOAaN89eCjz5eE@cluster0.j1x3i0z.mongodb.net/?retryWrites=true&w=majority";
 
-
-const DB = "mongodb+srv://user404:ZgXOAaN89eCjz5eE@cluster0.j1x3i0z.mongodb.net/?retryWrites=true&w=majority"
-
-
-mongoose.connect(DB, {
-    
-
-    
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("conection started")
-}).catch((err)=>{console.log(err.message)})
-
-
-
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("conection started");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
